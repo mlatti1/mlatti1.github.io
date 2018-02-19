68912799c984c4f06e05bc43ec26d27ad4f9db55
Lee Lattimore <leelattimore@gmail.com>
2018-02-18 18:12:16 -0600

    changed

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let oneNumber = 100
+let oneString = ‘hello world’
+let oneBoolean = true
+let oneArray = [‘dogs’, ‘cats’, ‘horse’]
+console.log(oneNumber)
+console.log(oneString)
+console.log(oneBoolean)
+console.log(oneArray)
