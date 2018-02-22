673dfdd912542c641c5bae20928bf36a8a124e03
Lee Lattimore <leelattimore@gmail.com>
2018-02-22 17:45:34 -0600

    code assignment 4

---
+++ code-assignment-04.js
@@ -0,0 +1,29 @@
+let ValueObject = {
+  1,
+words: {'astros', 'shamrock', 'apple'},
+numbers: {56},
+boolean: {false}
+url: 'https://mlatti1.github.io/',
+title: 'B Club',
+desc: 'Welcome to the B Club you bunch of squares!',
+thumb: true,
+keywords: [
+  'square', 'B', 'club', 'key'
+],
+true
+
+ }
+ let Array = [
+   1,
+   'https://mlatti1.github.io/',
+   'B Club',
+   'Welcome to the B Club you bunch of squares!',
+   true,
+   ['square', 'B', 'club', 'key']
+    ]
+
+
+
+ concolse.log(Array[1])
+ console.log(ValueObject.numbers)
+ console.log(ValueObject["numbers"])
