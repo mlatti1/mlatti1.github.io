b025b5b75292ca614e0bbf437dc0a081b42a3ca0
Lee Lattimore <leelattimore@gmail.com>
2018-02-26 22:53:49 -0600

    2-26-18
    69

---
+++ js1/code-assignment-05.js
@@ -0,0 +1,19 @@
+let project1 = {
+  id: 1,
+  title: 'Houston',
+  hasThumbnail: true
+}
+let project2 = {
+  id: 2
+  title: 'Earthquakes'
+  hasThumbnail: true
+}
+let projects = [
+  project1
+  project2
+]
+for (let i = 0; i < projects.length; i++) {
+  console.log('Element ' + i + ' title: ' + projects[i].title)
+  if (projects[i].hasThumbnail === true) {
+  console.log('Screenshot' + projects[i].id + '.png')
+  }
