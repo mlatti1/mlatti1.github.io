61395083f1092b1ae4bfe294aad851e8335bca3f
Lee Lattimore <leelattimore@gmail.com>
2018-05-01 19:43:32 -0500

    changed zoom

--- map/google/bonusscript.js
+++ map/google/bonusscript.js
@@ -1,7 +1,7 @@
 function initMap() {
   var mycenter = {lat: 29.757952, lng: -95.355600};
   var mymap = new google.maps.Map(document.getElementById('bonus'), {
-    zoom: 17,
+    zoom: 15,
     center: mycenter,
     mapTypeId: 'satellite'
   });
